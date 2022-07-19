9fa021621f17d9503346786a1de5c64c47d3035a
Soulether62 <rk7123745@gmail.com>
2022-07-19 10:43:35 +0530

    python codes

---
+++ Leap year.py
@@ -0,0 +1,5 @@
+n=int(input("Enter Year"))
+if n%4==0:
+    print('Its a Leap Year')
+else:
+    print('its not a leap Year')
---
+++ Prime number.py
@@ -0,0 +1,6 @@
+n=int(input('enter the number'))
+if n%n==0:
+    print('Its a prime number')
+else:
+    print('its not a prime number')
+    
