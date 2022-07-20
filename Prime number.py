50f8d27dc247944c988fe318de11056b612ed496
Soulether62 <rk7123745@gmail.com>
2022-07-20 09:07:09 +0530

    python codes

--- Prime number.py
+++ Prime number.py
@@ -1,6 +1,16 @@
-n=int(input('enter the number'))
-if n%n==0:
-    print('Its a prime number')
-else:
-    print('its not a prime number')
-    
+def PrimeChecker(a):    
+    if a > 1:  
+          
+        for j in range(2, int(a/2) + 1):  
+              
+            if (a % j) == 0:  
+                print(a, "is not a prime number")  
+                break  
+  
+        else:  
+            print(a, "is a prime number")  
+      
+    else:  
+        print(a, "is not a prime number")  
+a = int(input("Enter an input number:"))    
+PrimeChecker(a)   
